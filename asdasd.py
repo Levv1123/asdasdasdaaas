c1e77bed124892290f94fc1b5ae9410b096d553b
Levv1123 <90309810+Levv1123@users.noreply.github.com>
2021-10-06 10:16:17 +0200

    Add files via upload

---
+++ asdasd.py
@@ -0,0 +1,36 @@
+print("Feladat 1")
+nev= input ('mi a neved ? ')
+print ('Szia ', nev  )
+
+print ("Feladat 2")
+print ("**********")
+
+print ("Feladat 3")
+print ("*o*o*o*o*o")
+print ("*o*o*o*o*o")
+
+print ("Feladat 4")
+asd= int (input ('asd = '))
+dsa= int (input ('dsa = '))
+print ( asd + dsa )
+
+print ("Feladat 5")
+asd= int (input ('asd = '))
+dsa= int (input ('dsa = '))
+r= int (input ('r = '))
+print ( asd + dsa + r )
+
+print ("Feladat 6")
+asd= int (input ('asd = '))
+dsa= int (input ('dsa = '))
+print ( asd - dsa )
+
+print ("Feladat 7")
+asd= int (input ('asd = '))
+dsa= int (input ('dsa = '))
+print ( asd * dsa )
+
+print ("Feladat 8")
+asd= int (input ('asd = '))
+dsa= int (input ('dsa = '))
+print ( asd / dsa )
